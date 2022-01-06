08fd83f71f16d9a6a8dc13218fbcb0a2bf711ad1
Mike Ledkov <mledkov@gmail.com>
2022-01-06 15:52:03 +0300

    Complete 01-Color-Flipper

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,13 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById("btn");
+const colorName = document.querySelector(".color");
+btn.addEventListener("click", changeColor);
+
+function changeColor() {
+    const randomNumber = Math.round((colors.length - 1) * Math.random());
+    console.log(randomNumber);
+    const randomColor = colors[randomNumber];
+    document.body.style.backgroundColor = randomColor;
+    colorName.textContent = randomColor;
+};
