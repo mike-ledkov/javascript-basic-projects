522c0d58eff294fdd3dbfe2ce831431d9026080e
Mike Ledkov <mledkov@gmail.com>
2022-01-10 20:42:08 +0300

    Complete 04-navbar

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -3,3 +3,14 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+// Long but readable way
+// const navButton = document.querySelector(".nav-toggle");
+// const links = document.querySelector(".links");
+
+// navButton.addEventListener("click", function() {
+//     // links.classList.toggle("show-links");
+// })
+
+// Short but unreadable way
+document.querySelector(".nav-toggle").addEventListener("click", () => {document.querySelector(".links").classList.toggle("show-links");})
