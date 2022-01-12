0915e247950086dfe62fa3f38f3353f5c8005ecf
Mike Ledkov <mledkov@gmail.com>
2022-01-12 20:29:38 +0300

    Add comment to 07-questions app.js

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -14,16 +14,18 @@
 
 // I'm using getElementsByClassName instead of querySelectorAll
 const questions = document.getElementsByClassName("question");
-for (let i=0; i < questions.length; i++) {
-    openClose(questions[i]);
-}
+for (let i=0; i < questions.length; i++) { openClose(questions[i]); }
+
 function openClose(question) {
-    // question[i] selects ONE question
     // Select the button inside the question
-    // The button doesn't have to be a direct child of the question 
+    // The button doesn't have to be a direct child of the question
     let btn = question.querySelector(".question-btn");
+
     // Listen to the click on the button
     btn.addEventListener("click", function() {
+        // Cycle through the questions and if the current
+        // question is not the one that we clicked on,
+        // remove the show-text style
         for (let k = 0; k < questions.length; k++) {
             if (questions[k] !== question) {
                 questions[k].classList.remove("show-text");
