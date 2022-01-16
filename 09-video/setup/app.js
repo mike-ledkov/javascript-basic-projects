78433ce250472415255532840e282e27e028609a
Mike Ledkov <mledkov@gmail.com>
2022-01-16 14:50:29 +0300

    Complete 09-video
    Short project, but we learned to properly display video overlay and to use a preloader while the additional content loads.

--- 09-video/setup/app.js
+++ 09-video/setup/app.js
@@ -1,3 +1,22 @@
 // MDN
 // The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
 // The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
+
+const videoCont = document.querySelector(".video-container");
+const switchBtn = document.querySelector(".switch-btn");
+
+switchBtn.addEventListener("click", function() {
+    if(!switchBtn.classList.contains("slide")) {
+        switchBtn.classList.add("slide");
+        videoCont.pause();
+    } else {
+        switchBtn.classList.remove("slide");
+        videoCont.play();
+    }
+});
+
+const preloader = document.querySelector(".preloader");
+
+window.addEventListener("load", function() {
+    preloader.classList.add("hide-preloader");
+});
